ee7a011d88d17ae197095a6e881f25b7433f1d18
Mirko Nasato <mirko@nasato.dev>
2023-04-14 10:36:22 +0100

    Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
 const typeDefs = `#graphql
   type Query {
     greeting: String
@@ -9,3 +12,7 @@ const resolvers = {
     greeting: () => 'Hello world!',
   },
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
+console.log(`Server running at ${url}`);
